603337d5585c54a1549f86112f9a74175d9e07f5
Natalie Emanuel <natalieazana@gmail.com>
2018-07-13 18:19:03 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :size, :style, :color, :material, :condition
+  attr_reader :brand 
+  
+  def initialize(brand)
+    @brand = brand 
+  end 
+
+  def cobble
+    puts "The Shoe Has Been Repaired!"
+  end 
+  
+end 
+
+Shoe.new("You Have Big Feet!")
